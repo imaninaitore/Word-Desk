99124df4c9a3d76ccd4f21bc1161f8a0de822be5
imaninaitore <imaninaitore1726@gmail.com>
2026-08-12 13:26:14 +0300

    feat: remove unecessary print,extract part of speech ,definition and example and print accordingly

--- newstore.py
+++ newstore.py
@@ -10,23 +10,37 @@ url = f"https://raw.githubusercontent.com/mhollingshead/open-dictionary/main/api
 
 response = requests.get(url)
 
-print(response.status_code)
+#print(response.status_code)#no need to print the status code, this is for testing 
 
 if response.status_code == 200:
 
     data = response.json()
 
-    print(data[word])
+    # print(data[word])# removed this to print in the proper layout
 
 else:
 
     print("Could not find the dictionary file.")
 
     #extraction section
+data = response.json() 
 word_data = data[word]
 word = word_data["word"]
+print(f"Word:{word}")
+print()
 
 for etymology in word_data["etymologies"]:
   for details in etymology["partsOfSpeech"]:
+    print()
 
     part_of_speech = details["partOfSpeech"]
+    print(f"part_of_speech:{part_of_speech}")
+
+    for definition in details["senses"]:
+
+      definition_text = definition["sense"]
+      print(f"Definition:{definition_text}")
+
+    for example in definition["examples"]:
+
+      print(f"Example:{example}")    
