05744c18ea2998b6eb7dc7fbc5f12836e9da1c85
imaninaitore <imaninaitore1726@gmail.com>
2026-08-13 11:10:54 +0300

    feat:change the url structure to get all files based on the letter arrangement they exist in

--- populate.py
+++ populate.py
@@ -1,49 +1,3 @@
-# word = input("Search for a word: ").lower()
-
-# first_letter = word[0]
-# first_two_letters = word[:2]
-
-# url = f"https://raw.githubusercontent.com/mhollingshead/open-dictionary/main/api/{first_letter}/{first_two_letters}.json"
-
-# response = requests.get(url)
-
-# #print(response.status_code)#no need to print the status code, this is for testing 
-
-# if response.status_code == 200:
-
-#     data = response.json()
-
-#     #print(data[word])# removed this to print in the proper layout
-
-# else:
-
-#     print("Could not find the dictionary file.")
-
-# #extraction section
-# data = response.json() 
-# word_data = data[word]
-# word = word_data["word"]
-# print(f"Word:{word}")
-# print()
-
-
-# for etymology in word_data["etymologies"]:
-#   for details in etymology["partsOfSpeech"]:
-#     print()
-
-#     part_of_speech = details["partOfSpeech"]
-#     print(f"part_of_speech:{part_of_speech}")
-
-#     for definition in details["senses"]:
-
-#       definition_text = definition["sense"]
-#       print(f"Definition:{definition_text}")
-
-#     for example in definition["examples"]:
-
-#       print(f"Example:{example}")    
-
-
 #SQLITE DATABASE STORAGE
 import requests
 import sqlite3
@@ -62,9 +16,15 @@ CREATE TABLE IF NOT EXISTS words (
 """)
 
 
-url = "https://raw.githubusercontent.com/mhollingshead/open-dictionary/main/api/a/ab.json"
+url = "https://raw.githubusercontent.com/mhollingshead/open-dictionary/contents/api"
 
-response = requests.get(url)
+letters ="abcdefghijklmnopqrstuvwxyz"
+
+for letter in letters:
+
+    folder_url = f"{url}/{letter}"
+
+    response = requests.get(folder_url)
 
 if response.status_code == 200:
 
