397b8d97ad53e624d09c51d9d21dc6b77ec92ac9
imaninaitore <imaninaitore1726@gmail.com>
2026-08-14 08:16:08 +0300

    create the pages

--- main.py
+++ main.py
@@ -10,4 +10,10 @@ window.geometry("700x500")
 notebook = ttk.Notebook(window)
 notebook.pack( fill="both",expand=True)
 
-
+#create the pages
+signin_page=tk.Frame(notebook)
+login_page=tk.Frame(notebook)
+instructions_page = tk.Frame(notebook)
+home_page = tk.Frame(notebook)
+quiz_page = tk.Frame(notebook)
+learning_page = tk.Frame(notebook)
