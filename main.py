f444317cdd4fcc6ab3503eec2e4c858ead471d18
imaninaitore <imaninaitore1726@gmail.com>
2026-08-14 08:33:38 +0300

    create container for the pages

--- main.py
+++ main.py
@@ -7,19 +7,14 @@ from pages.home import HomePage
 from pages.quiz import QuizPage
 from pages.learning import LearningPage
 
-# ==========================================
 # MAIN WINDOW
-# ==========================================
-
 app = ctk.CTk()
 
 app.title("WordDesk")
 app.geometry("900x650")
 app.resizable(False, False)
 
-
-# CONTAINER
-
+# CONTAINER- where all pages will be
 container = ctk.CTkFrame(
     app,
     fg_color="transparent"
@@ -30,57 +25,3 @@ container.pack(
     expand=True
 )
 
-
-# PAGE MANAGER
-
-pages = {}
-
-
-def show_page(page_name):
-
-    page = pages[page_name]
-
-    page.tkraise()
-
-
-# CREATE PAGES
-
-
-pages["home"] = HomePage(
-    container,
-    show_page
-)
-
-pages["quiz"] = QuizPage(
-    container,
-    show_page
-)
-
-pages["dictionary"] = DictionaryPage(
-    container,
-    show_page
-)
-
-pages["favorites"] = FavoritesPage(
-    container,
-    show_page
-)
-
-
-# POSITION ALL PAGES
-
-for page in pages.values():
-
-    page.place(
-        x=0,
-        y=0,
-        relwidth=1,
-        relheight=1
-    )
- 
-# START PAGE
-
-show_page("home")
-
-# RUN
-app.mainloop()
